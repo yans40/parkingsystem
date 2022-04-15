0bd3a167e45417230dcd311be57908b49c672b1f
yannick <yandollou@hotmail.com>
2022-04-15 09:13:45 +0200

    maj du pom xml maj de la méthode de calcul de gratuité maj de jacoco afin d'en tirer un rapport

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -21,7 +21,7 @@ public class InputReaderUtil {
         }
     }
 
-    public String readVehicleRegistrationNumber() throws Exception {
+    public String readVehicleRegis trationNumber() throws Exception {
         try {
             String vehicleRegNumber = scan.nextLine();
             if (vehicleRegNumber == null || vehicleRegNumber.trim().length() == 0) {
