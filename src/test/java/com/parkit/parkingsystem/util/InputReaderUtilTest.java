cec2f2b8b6044bf5ffe2feafb3e9a16abae575b7
yannick <yandollou@hotmail.com>
2022-06-03 14:17:49 +0200

    maj

--- src/test/java/com/parkit/parkingsystem/util/InputReaderUtilTest.java
+++ src/test/java/com/parkit/parkingsystem/util/InputReaderUtilTest.java
@@ -10,26 +10,4 @@ import java.util.Scanner;
 
 class InputReaderUtilTest {
 
-
-  /*  private static InputReaderUtil inputReaderUtil = new InputReaderUtil();
-    private static Scanner scan = new Scanner(System.in);*/
-
-
-    @Test
-    void ReadWrightVehicleRegistrationNumber() throws Exception {
-
-
-//ARRANGE
-    /*    String input = "ABCDEF";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        String vehicleRegNumber = scan.nextLine();
-
-//ACT
-        inputReaderUtil.readVehicleRegistrationNumber();
-
-//ASSERT
-
-        Assertions.assertEquals("ABCDEF", inputReaderUtil.readVehicleRegistrationNumber());*/
-    }
 }
